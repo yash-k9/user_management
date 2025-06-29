a1b7b06b9d297b4c6806d6bb5a2d73921001db35
Kurella Yash <yashkurella.9@gmail.com>
2025-06-29 17:05:06 +0530

    add method channel for user add event

--- android/app/src/main/kotlin/com/task/user_management/MainActivity.kt
+++ android/app/src/main/kotlin/com/task/user_management/MainActivity.kt
@@ -1,5 +1,40 @@
 package com.task.user_management
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, EVENT_CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                ADD_USER_ACTIVITY -> {
+                    try {
+                        val intent = Intent(this, AddUserActivity::class.java).apply {
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+                        startActivity(intent)
+                    } catch (e: Exception) {
+                        result.error("ACTIVITY_ERROR", e.localizedMessage, null)
+                    }
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val EVENT_CHANNEL = "nativeChannel"
+        const val USER_STREAM_CHANNEL = "userStreamChannel"
+        const val ADD_USER_ACTIVITY = "add_user"
+    }
+}
